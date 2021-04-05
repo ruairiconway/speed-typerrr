f55e23e81ec705ba5798d38c8458d155e73d92d3
Ruairi Conway <hi@ruairiconway.com>
2021-04-05 13:02:24 -0400

    added custom hook

---
+++ src/hooks/useWordGame.js
@@ -0,0 +1,47 @@
+import { useState, useEffect, useRef } from 'react'
+
+function useWordGame() {
+    const START_TIME = 5
+    const [ textData, setTextData ] = useState('')
+    const [ timerCount, setCount ] = useState(START_TIME)
+    const [ isTimerRunning, setIsTimerRunning ] = useState(false)
+    const [ wordCount, setWordCount ] = useState(0)
+    const textareaRef = useRef(null)
+
+    function handleTextChange(e) {
+        const { value } = e.target
+        setTextData(value)
+    }
+
+    function handleWordCount(textStr) {
+        const inputArray = textStr.trim().split(' ')
+        const filteredWordCount = inputArray.filter(word => word !== '').length
+        return filteredWordCount
+    }
+
+    async function handleGameStart() {   
+        setTextData('')
+        setCount(START_TIME)
+        await setIsTimerRunning(true)
+        textareaRef.current.focus()
+    }
+
+    function handleGameEnd() {
+        setIsTimerRunning(false)
+        setWordCount(handleWordCount(textData))
+    }
+
+    useEffect(() => {
+        if (timerCount > 0 && isTimerRunning) {
+            setTimeout( () => {
+                setCount(prevTimerCount => prevTimerCount - 1)
+            }, 1000)
+        } else if (timerCount === 0) {
+            handleGameEnd()
+        }
+    }, [timerCount, isTimerRunning])
+
+    return {textareaRef, textData, handleTextChange, isTimerRunning, timerCount, handleGameStart, wordCount}
+}
+
+export default useWordGame
