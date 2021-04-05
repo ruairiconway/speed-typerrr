bbc188cb4d4f46b2b581b7f9dbc9ef07483d320e
Ruairi Conway <hi@ruairiconway.com>
2021-04-05 11:29:49 -0400

    minimal viable product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css';
 
 function App() {
+    const START_TIME = 5
     const [ textData, setTextData ] = useState('')
-    const [ timerCount, setCount ] = useState(5)
+    const [ timerCount, setCount ] = useState(START_TIME)
+    const [ isTimerRunning, setIsTimerRunning ] = useState(false)
+    const [ wordCount, setWordCount ] = useState(0)
+    const textareaRef = useRef(null)
 
     function handleTextChange(e) {
         const { value } = e.target
@@ -16,24 +20,45 @@ function App() {
         return filteredWordCount
     }
 
+    async function handleGameStart() {   
+        setTextData('')
+        setCount(START_TIME)
+        await setIsTimerRunning(true)
+        textareaRef.current.focus()
+    }
+
+    function handleGameEnd() {
+        setIsTimerRunning(false)
+        setWordCount(handleWordCount(textData))
+    }
+
     useEffect(() => {
-        timerCount > 0 &&
-        setTimeout( () => {
-            setCount(prevTimerCount => prevTimerCount - 1)
-        }, 1000)
-    }, [timerCount])
+        if (timerCount > 0 && isTimerRunning) {
+            setTimeout( () => {
+                setCount(prevTimerCount => prevTimerCount - 1)
+            }, 1000)
+        } else if (timerCount === 0) {
+            handleGameEnd()
+        }
+    }, [timerCount, isTimerRunning])
 
     return (
         <div>
             <h1>SPEED TYPERRR</h1>
             <textarea
+                ref={textareaRef}
                 name="gameInput"
                 value={textData}
                 onChange={handleTextChange}
+                disabled={!isTimerRunning}
             />
             <h4>TIME: {timerCount}</h4>
-            <button onClick={() => handleWordCount(textData)} type="submit">START</button>
-            <h1>WORD COUNT: ???</h1>
+            <button
+                onClick={handleGameStart} 
+                type="button" 
+                disabled={isTimerRunning}
+            >START</button>
+            <h1>WORD COUNT: {wordCount}</h1>
         </div>
     );
 }
